68dfb35fb9a92b6d1b812ed822ebb115d6332644
Samuel Legrain <samuel.legrain@bstorm.be>
2025-09-22 12:32:48 +0200

    Init Routing : Base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Demo1 } from './components/pages/demo1/demo1';
+import { Demo2 } from './components/pages/demo2/demo2';
+import { Home } from './components/pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', redirectTo : 'home', pathMatch : 'full'},
+    { path : 'home', component : Home},
+    { path : 'demo1', component : Demo1 },
+    { path : 'demo2', component : Demo2 }
+];
